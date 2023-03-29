2ff27d5a422c94a501d2656698664ba12ba7bd83
Naomi Timothy <naomitim17.com>
2023-03-29 11:01:14 +0300

    landing--home

--- guzo/src/App.js
+++ guzo/src/App.js
@@ -1,11 +1,23 @@
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
 	return (
-		<div className="App">
-			<NavBar />
-			<div className="content"></div>
-		</div>
+		<Router>
+			<div className="App">
+				<NavBar />
+				<div className="content">
+					<Routes>
+						<Route exact path="/" Component={Home} />
+						<Route exact path="/travel" Component={Home} />
+						<Route exact path="/favorites" Component={Home} />
+						<Route exact path="/review" Component={Home} />
+						<Route exact path="/profile" Component={Home} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
--- guzo/src/pages/Home.js
+++ guzo/src/pages/Home.js
@@ -1,5 +1,46 @@
+import camping2 from "../assets/camping2.svg";
 const Home = () => {
-	return <div className="landing"></div>;
+	const platform = new URL("../assets/platforms.png", import.meta.url);
+	const review = new URL("../assets/reviews.png", import.meta.url);
+	const guidelines = new URL("../assets/guidelines.png", import.meta.url);
+
+	return (
+		<div className="home">
+			<div className="main-landing">
+				<div className="landing">
+					<div className="textual">
+						<div className="text">
+							<h2>Travel to any corner of the world</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+								nemo quibusdam laudantium tempore sit eius, autem illum quos
+								placeat natus qui ut alias nisi perferendis. Et voluptas
+								deserunt quia aperiam
+							</p>
+						</div>
+						<button className="action-btn">Explore Now</button>
+					</div>
+					<div className="graphical">
+						<img src={camping2} alt="" />
+					</div>
+				</div>
+				<div className="info">
+					<div className="info-content">
+						<img src={review} alt="review icon" />
+						<p>Reliable Reviews</p>
+					</div>
+					<div className="info-content">
+						<img src={platform} alt="review icon" />
+						<p>Access From Any Platform</p>
+					</div>
+					<div className="info-content">
+						<img src={guidelines} alt="review icon" />
+						<p>Guides & Community</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
